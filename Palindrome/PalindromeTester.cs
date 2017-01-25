6218cd80453c209edc158a8efe5ed53474bd1753
Kevin J Donohue <kevinjdonohue@yahoo.com>
2017-01-25 00:49:41 -0800

    Add Palindrome solution

---
+++ Palindrome/PalindromeTester.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+
+namespace Palindrome
+{
+    public class PalindromeTester
+    {
+        public bool IsPalindromeWord(string word)
+        {
+            int endIndex = word.Length - 1;
+
+            for (int i = 0; i < word.Length / 2; i++)
+            {
+                char startCharacter = word[i];
+                char endCharacter = word[endIndex - i];
+
+                if (startCharacter != endCharacter)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public bool IsPalindromeSentence(string sentence)
+        {
+            return IsPalindromeWord(CleanSentenceAndConvertToAWord(sentence));
+        }
+
+        private string CleanSentenceAndConvertToAWord(string sentence)
+        {
+            StringBuilder candidate = new StringBuilder();
+
+            foreach (char currentCharacter in sentence)
+            {
+                if (char.IsLetterOrDigit(currentCharacter))
+                {
+                    candidate.Append(char.ToLower(currentCharacter));
+                }
+            }
+            return candidate.ToString();
+        }
+    }
+}
---
+++ PalindromeTests/PalindromeTesterTests.cs
@@ -0,0 +1,64 @@
+﻿using FluentAssertions;
+using Palindrome;
+using Xunit;
+
+namespace CheckerTests
+{
+    public class PalindromeTesterTests
+    {
+        [Theory]
+        [InlineData("civic")]
+        [InlineData("deified")]
+        [InlineData("deleveled")]
+        [InlineData("devoved")]
+        [InlineData("hannah")]
+        [InlineData("kayak")]
+        [InlineData("level")]
+        [InlineData("reviver")]
+        public void IsPalindromeWord_ShouldReturnTrue_GivenAValidWord(string word)
+        {
+            //arrange
+            PalindromeTester tester = new PalindromeTester();
+
+            //act
+            bool result = tester.IsPalindromeWord(word);
+
+            //assert
+            result.Should().BeTrue($"because {word} is a palindrome");
+        }
+
+        [Theory]
+        [InlineData("civiC")]
+        [InlineData("deifieD")]       
+        [InlineData("Hannah")]
+        [InlineData("leveled")]        
+        public void IsPalindromeWord_ShouldReturnFalse_GivenAValidPalidrome(string word)
+        {
+            //arrange
+            PalindromeTester tester = new PalindromeTester();
+
+            //act
+            bool result = tester.IsPalindromeWord(word);
+
+            //assert
+            result.Should().BeFalse($"because {word} is not a palindrome");
+        }
+
+        [Theory]
+        [InlineData("A man, a plan, a canal: Panama.")]
+        [InlineData("A Toyota. Race fast, safe car. A Toyota.")]
+        [InlineData("Cigar? Toss it in a can. It is so tragic.")]
+        [InlineData("Murder for a jar of red rum.")]
+        public void IsPalindromeSentence_ShouldReturnTrue_GivenAValidSentence(string sentence)
+        {
+            //arrange
+            PalindromeTester tester = new PalindromeTester();
+
+            //act
+            bool result = tester.IsPalindromeSentence(sentence);
+
+            //assert
+            result.Should().BeTrue($"because {sentence} is a palindrome");
+        }
+    }
+}
